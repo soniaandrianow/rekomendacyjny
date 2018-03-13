b2069c7ddd95c7c02a73d7daa08dcd2c4d03b6d1
Sonia Andrianow <sonia.andrianow@gmail.com>
2018-03-13 14:46:27 +0100

    'statystyczne'

--- console/controllers/TestController.php
+++ console/controllers/TestController.php
@@ -9,7 +9,35 @@
 namespace console\controllers;
 
 
-class TestController
+use Phpml\Classification\NaiveBayes;
+use Phpml\Classification\SVC;
+use Phpml\SupportVectorMachine\Kernel;
+use yii\console\Controller;
+
+class TestController extends Controller
 {
+    public function actionBayes()
+    {
+        $samples = [[1.5, 2.1, 1.2], [2.3, 3.0, 1.0], [4.3, 3.8, 5.1]];
+        $labels = ['1', '2', '4'];
+
+        $classifier = new NaiveBayes();
+        $classifier->train($samples, $labels);
+
+        $prediction = $classifier->predict([2.2, 4.5, 5.0]);
+        var_dump($prediction); die;
+    }
+
+    public function actionSvc()
+    {
+        $samples = [[1.5, 2.1, 1.2], [2.3, 3.0, 1.0], [4.3, 3.8, 5.1]];
+        $labels = ['1', '2', '4'];
+
+        $classifier = new SVC(Kernel::RBF, $cost = 1000, $degree = 3, $gamma = 6);
+        $classifier->train($samples, $labels);
+
+        $prediction = $classifier->predict([2.2, 4.5, 5.0]);
+        var_dump($prediction); die;
+    }
 
 }
